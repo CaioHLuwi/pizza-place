38642ddc8a079d2aaa1a2d40261c54c658298b9b
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-12-13 16:19:07 -0300

    feat: Add item to card & all pizza information to item and + & - on quantity funcionality

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ const qa = (el) => document.querySelectorAll(el);
 let cart = [];
 let modalQt = 1;
 let modalKey = 0;
+let cartQt = 1;
 
-let pizzaQt = document.querySelector('.pizzaInfo--qt');
-let buttonLess = document.querySelector('.pizzaInfo--qtmenos');
-let buttonMore = document.querySelector('.pizzaInfo--qtmais');
 let buttonAddCart = q('.pizzaInfo--addButton');
 
 pizzaJson.map((item, index) => {
@@ -43,7 +41,7 @@ pizzaJson.map((item, index) => {
         let pizzaSize = pizzaInfo.querySelectorAll('.pizzaInfo--size');
         pizzaSize.forEach((size, index) => {
 
-            if(index == 2) {
+            if (index == 2) {
                 size.classList.add('selected');
             }
 
@@ -70,16 +68,19 @@ function closeModal() {
     }, 300)
 }
 
-buttonLess.addEventListener('click', () => {
-    if(modalQt > 1) {
+let pizzaQt = document.querySelector('.pizzaInfo--qt');
+let buttonLessModal = document.querySelector('.pizzaInfo--qtmenos');
+buttonLessModal.addEventListener('click', () => {
+    if (modalQt > 1) {
         modalQt--;
     }
-    
+
     pizzaQt.innerHTML = modalQt
 });
 
-buttonMore.addEventListener('click', () => {
-    if(modalQt >= 0) {
+let buttonMoreModal = document.querySelector('.pizzaInfo--qtmais');
+buttonMoreModal.addEventListener('click', () => {
+    if (modalQt >= 0) {
         modalQt++;
     }
 
@@ -95,7 +96,7 @@ buttonAddCart.addEventListener('click', () => {
     let qtd = modalQt;
     let identifier = pizzaJson[modalKey].id + '@' + size;
     let key = cart.findIndex((item) => item.identifier == identifier);
-    if(key > -1) {
+    if (key > -1) {
         cart[key].qtd += qtd;
     } else {
         cart.push({
@@ -111,29 +112,53 @@ buttonAddCart.addEventListener('click', () => {
 });
 
 function updateCart() {
-    if(cart.length > 0) {
-        let pizzaItem = ''
+    if (cart.length > 0) {
+        let pizzaItem = '';
+        q('.cart').innerHTML = '';
         q('aside').classList.add('show');
-        for(let i in cart) {
+
+        for (let i in cart) {
             pizzaItem = pizzaJson.find(item => item.id == cart[i].id);
+            let pizzaCartItem = document.querySelector('.cart--item').cloneNode(true);
+            let pizzaSizeName;
+            switch (cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+                default:
+                    pizzaSize = 'G';
+                    break;
+            }
 
-            console.log(pizzaItem);
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`
+
+            pizzaCartItem.querySelector('img').src = pizzaItem.img;
+            pizzaCartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            pizzaCartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qtd;
+            pizzaCartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qtd > 1) {
+                    cart[i].qtd--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            })
+            pizzaCartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qtd++;
+                updateCart();
+
+            })
+
+            let pizzaCartArea = document.querySelector('.cart');
+            pizzaCartArea.append(pizzaCartItem);
         }
-
-        let pizzaCartItem = document.querySelector('.cart--item').cloneNode(true);
-        let pizzaCartArea = document.querySelector('.cart');
-
-        pizzaCartItem.querySelector('img').src = pizzaItem.img;
-        pizzaCartItem.querySelector('.cart--item-nome').innerHTML = pizzaItem.name;
-        for(i in cart) {
-            pizzaCartItem.querySelector('.cart--item--qtarea .cart--item--qt').innerHTML = cart[i].qtd;
-        }
-        
-
-
-
-        pizzaCartArea.append(pizzaCartItem);
-    } else {    
+    } else {
         q('aside').classList.remove('show');
     }
 };
