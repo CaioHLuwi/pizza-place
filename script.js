cd4700a4d22303f4611b0bba173352db1c5ef4ba
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-12-12 23:14:04 -0300

    feat: Add modal onclick in plus button, add data to fields in modal

--- script.js
+++ script.js
@@ -9,6 +9,36 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let modalItem = q('.pizzaWindowArea')
+        modalItem.style.opacity = 0;
+        modalItem.style.display = 'flex';
+        setTimeout(() => {
+            modalItem.style.opacity = 1;
+        })
+
+        modalItem.querySelector('.pizzaBig img').src = item.img;
+        let pizzaInfo = modalItem.querySelector('.pizzaInfo');
+
+        pizzaInfo.querySelector('h1').innerHTML = item.name;
+        pizzaInfo.querySelector('.pizzaInfo--desc').innerHTML = item.description;
+        pizzaInfo.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+        let pizzaSize = pizzaInfo.querySelectorAll('.pizzaInfo--size');
+        pizzaSize.forEach((size, index) => {
+            size.addEventListener('click', (e) => {
+                pizzaSize.forEach(item => item.classList.remove('selected'));
+
+                if(e.target.classList.contains('selected')) {
+                    e.target.classList.remove('selected');
+                } else {
+                    e.target.classList.add('selected');
+                }
+            })
+        })
+    })
 
 
     pizzaArea.append(pizzaItem)
