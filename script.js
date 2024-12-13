7ff91b8da89f6bb67423cab7ec5822e47f17636a
CaioHLuwi <caiohenriquelw2004@gmail.com>
2024-12-13 10:21:11 -0300

    feat: Add logic of add pizza to cart & close modal/add quantity pizza dinamic add or reduce.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const q = (el) => document.querySelector(el);
 const qa = (el) => document.querySelectorAll(el);
 
+let cart = [];
+let modalQt = 1;
+let modalKey = 0;
+
+let pizzaQt = document.querySelector('.pizzaInfo--qt');
+let buttonLess = document.querySelector('.pizzaInfo--qtmenos');
+let buttonMore = document.querySelector('.pizzaInfo--qtmais');
+let buttonAddCart = q('.pizzaInfo--addButton');
+
 pizzaJson.map((item, index) => {
     let pizzaItem = q('.models .pizza-item').cloneNode(true);
     let pizzaArea = q('.pizza-area');
 
+    pizzaItem.setAttribute('data-key', index)
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        modalKey = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
+
 
         let modalItem = q('.pizzaWindowArea')
         modalItem.style.opacity = 0;
@@ -25,21 +39,81 @@ pizzaJson.map((item, index) => {
         pizzaInfo.querySelector('h1').innerHTML = item.name;
         pizzaInfo.querySelector('.pizzaInfo--desc').innerHTML = item.description;
         pizzaInfo.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`;
-
+        pizzaInfo.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
         let pizzaSize = pizzaInfo.querySelectorAll('.pizzaInfo--size');
         pizzaSize.forEach((size, index) => {
-            size.addEventListener('click', (e) => {
-                pizzaSize.forEach(item => item.classList.remove('selected'));
 
-                if(e.target.classList.contains('selected')) {
-                    e.target.classList.remove('selected');
-                } else {
-                    e.target.classList.add('selected');
-                }
+            if(index == 2) {
+                size.classList.add('selected');
+            }
+
+            size.querySelector('span').innerHTML = item.sizes[index];
+
+            size.addEventListener('click', (e) => {
+                q('.pizzaInfo--size.selected').classList.remove('selected');
+                size.classList.add('selected');
             })
         })
+
+        pizzaQt.innerHTML = modalQt;
     })
 
 
     pizzaArea.append(pizzaItem)
 });
+
+function closeModal() {
+    document.querySelector('.pizzaWindowArea').style.opacity = 0;
+
+    setTimeout(() => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none';
+    }, 300)
+}
+
+buttonLess.addEventListener('click', () => {
+    if(modalQt > 1) {
+        modalQt--;
+    }
+    
+    pizzaQt.innerHTML = modalQt
+});
+
+buttonMore.addEventListener('click', () => {
+    if(modalQt >= 0) {
+        modalQt++;
+    }
+
+    pizzaQt.innerHTML = modalQt
+});
+
+qa('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+})
+
+buttonAddCart.addEventListener('click', () => {
+    let size = parseInt(q('.pizzaInfo--size.selected').getAttribute('data-key'));
+    let qtd = modalQt;
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+    let key = cart.findIndex((item) => item.identifier == identifier);
+    if(key > -1) {
+        cart[key].qtd += qtd;
+    } else {
+        cart.push({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            size,
+            qtd,
+        });
+    }
+
+    updateCart();
+    closeModal();
+});
+
+function updateCart() {
+    if(cart.length > 0) {
+        q('aside').classList.add('show');
+    } else {
+        q('aside').classList.remove('show');
+    }
+};
